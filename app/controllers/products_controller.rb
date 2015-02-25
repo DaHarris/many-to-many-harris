0b2f450810f220ad02cdf1d7f8c0d6eb121dcd90
Daniel Harris <bobth3bum@gmail.com>
2015-02-25 14:05:30 -0700

    add products new and create with company dropdown

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,8 +8,18 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
-
   def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path, notice: "Product was successfully created."
+    else
+      render :new
+    end
   end
 
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :company_id)
+  end
 end
