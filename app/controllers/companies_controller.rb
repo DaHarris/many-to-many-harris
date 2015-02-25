99675ae3ad6af99d2dfadb843178149c90f0a769
Daniel Harris <bobth3bum@gmail.com>
2015-02-25 13:42:06 -0700

    add create functionality for company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -13,4 +13,19 @@ class CompaniesController < ApplicationController
     @company = Company.new
   end
 
+  def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to companies_path, notice: "Company was succesfully created."
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :catch_phrase, :suffix)
+  end
+
 end
