29aed6bb0e6d8a7cb358e7965cd1f67d502a663a
Daniel Harris <bobth3bum@gmail.com>
2015-02-25 13:33:16 -0700

    add show company page

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -4,4 +4,9 @@ class CompaniesController < ApplicationController
     @companies = Company.all
   end
 
+  def show
+    @company = Company.find(params[:id])
+    @products = @company.products
+  end
+
 end
