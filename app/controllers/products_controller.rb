13c4cd6e17e59813af0438e0225b728dbcbbba7c
Daniel Harris <bobth3bum@gmail.com>
2015-02-25 21:02:06 -0700

    add products edit page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -27,6 +27,24 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if !@product.valid?
+      render :edit
+    else
+      if @product.update(product_params)
+        redirect_to products_path, notice: "Product was successfully updated."
+      else
+        render :edit
+      end
+    end
+  end
+
+
   private
 
   def product_params
