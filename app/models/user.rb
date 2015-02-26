e5d5ffe6959a366af55c73d30c848c65ba4d9a1e
Daniel Harris <bobth3bum@gmail.com>
2015-02-25 22:04:29 -0700

    add bcrypt and model has_secure_password and user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :company_reviews
   has_many :product_reviews
+  validates :name, :email, :password, presence: true
+  validates :email, uniqueness: true
 
 end
